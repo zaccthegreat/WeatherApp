d9b2be7881c7cd7d7382e952232a07ceb2ca118c
Zacchaeus Day <zaccthegreat@gmail.com>
2018-02-28 19:23:36 -0500

    site update

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+/*
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === './Fayette.png') {
+      myImage.setAttribute ('src','./Kentucky.jpg');
+    } else {
+      myImage.setAttribute ('src','./Fayette.png');
+    }
+}
+
+alert('Hello Welcome to My Site!')
+document.write('This is a test of document.write')
+
+var message = "Hello!";
+alert(message);
+*/
